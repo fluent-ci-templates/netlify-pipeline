68ec41bd21942f6e59174005038221bc7c0671cf
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-09-16 05:27:46 +0000

    use bun@1.0.0
    run `netlify-cli link`
    
    run `netlify-cli status`
    
    remove devbox cache
    
    update env
    
    update github actions workflow
    
    update github actions workflow
    
    debug netlify
    
    don't exclude .git directory
    
    remove --debug option
    
    fix netlify build context
    
    remove example
    
    remove example

--- example/.fluentci/src/dagger/index.ts
+++
@@ -1,4 +0,0 @@
-import pipeline from "./pipeline.ts";
-import { build, deploy } from "./jobs.ts";
-
-export { pipeline, build, deploy };
--- src/dagger/jobs.ts
+++ src/dagger/jobs.ts
@@ -1,42 +1,30 @@
 import Client from "@fluentci.io/dagger";
-import { withDevbox } from "https://nix.fluentci.io/v0.4.1/src/dagger/steps.ts";
 
 export enum Job {
   build = "build",
   deploy = "deploy",
 }
 
-export const exclude = [".git", ".devbox", "node_modules", ".fluentci"];
+export const exclude = [".devbox", "node_modules", ".fluentci"];
 
 export const build = async (client: Client, src = ".") => {
   const context = client.host().directory(src);
-  const ctr = withDevbox(
-    client
-      .pipeline(Job.build)
-      .container()
-      .from("alpine:latest")
-      .withExec(["apk", "update"])
-      .withExec(["apk", "add", "curl", "bash"])
-      .withMountedCache("/nix", client.cacheVolume("nix"))
-      .withMountedCache("/etc/nix", client.cacheVolume("nix-etc"))
-  )
-    .withMountedCache(
-      "/root/.local/share/devbox/global",
-      client.cacheVolume("devbox-global")
-    )
-    .withExec(["devbox", "global", "add", "nodejs@18.16.1", "bun@1.0.0"])
+  const ctr = client
+    .pipeline(Job.build)
+    .container()
+    .from("ghcr.io/fluent-ci-templates/bun:latest")
     .withMountedCache(
       "/root/.bun/install/cache",
       client.cacheVolume("bun-cache")
     )
     .withMountedCache("/app/node_modules", client.cacheVolume("node_modules"))
-    .withEnvVariable("NIX_INSTALLER_NO_CHANNEL_ADD", "1")
     .withDirectory("/app", context, { exclude })
     .withWorkdir("/app")
+    .withExec(["sh", "-c", 'eval "$(devbox global shellenv)" && bun install'])
     .withExec([
       "sh",
       "-c",
-      'eval "$(devbox global shellenv)" && bun install && bun run build',
+      'eval "$(devbox global shellenv)" && bun run build',
     ]);
 
   const result = await ctr.stdout();
@@ -49,45 +37,33 @@ export const build = async (client: Client, src = ".") => {
 export const deploy = async (client: Client, src = ".") => {
   const context = client.host().directory(src);
 
-  if (!Deno.env.get("NETLIFY_AUTH_TOKEN")) {
+  if (!Deno.env.has("NETLIFY_AUTH_TOKEN")) {
     console.log("NETLIFY_AUTH_TOKEN is not set");
     Deno.exit(1);
   }
 
-  if (!Deno.env.get("NETLIFY_SITE_ID")) {
+  if (!Deno.env.has("NETLIFY_SITE_ID")) {
     console.log("NETLIFY_SITE_ID is not set");
     Deno.exit(1);
   }
 
   const dir = Deno.env.get("NETLIFY_SITE_DIR") || ".";
 
-  let deployCommand = `eval "$(devbox global shellenv)" && bun x netlify-cli deploy --dir ${dir}`;
+  let deployCommand = `eval "$(devbox global shellenv)" && bun x netlify-cli status && bun x netlify-cli deploy --dir ${dir}`;
 
   if (Deno.env.get("PRODUCTION_DEPLOY") === "1") {
     deployCommand += " --prod";
   }
 
-  const ctr = withDevbox(
-    client
-      .pipeline(Job.deploy)
-      .container()
-      .from("alpine:latest")
-      .withExec(["apk", "update"])
-      .withExec(["apk", "add", "curl", "bash"])
-      .withMountedCache("/nix", client.cacheVolume("nix"))
-      .withMountedCache("/etc/nix", client.cacheVolume("nix-etc"))
-  )
-    .withMountedCache(
-      "/root/.local/share/devbox/global",
-      client.cacheVolume("devbox-global")
-    )
-    .withExec(["devbox", "global", "add", "nodejs@18.16.1", "bun@1.0.0"])
+  const ctr = client
+    .pipeline(Job.deploy)
+    .container()
+    .from("ghcr.io/fluent-ci-templates/bun:latest")
     .withMountedCache(
       "/root/.bun/install/cache",
       client.cacheVolume("bun-cache")
     )
     .withMountedCache("/app/node_modules", client.cacheVolume("node_modules"))
-    .withEnvVariable("NIX_INSTALLER_NO_CHANNEL_ADD", "1")
     .withEnvVariable("NETLIFY_AUTH_TOKEN", Deno.env.get("NETLIFY_AUTH_TOKEN")!)
     .withEnvVariable("NETLIFY_SITE_ID", Deno.env.get("NETLIFY_SITE_ID")!)
     .withDirectory("/app", context, { exclude })
