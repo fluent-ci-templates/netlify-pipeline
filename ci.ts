8c18e92d45ae83151d0fa9ecb9c5b652e4b8ae99
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-12-17 22:17:29 +0000

    use bun@1.0.0

--- ci.ts
+++ ci.ts
@@ -1,7 +1,7 @@
 import {
   build,
   deploy,
-} from "https://pkg.fluentci.io/netlify_pipeline@v0.7.0/mod.ts";
+} from "https://pkg.fluentci.io/netlify_pipeline@v0.7.1/mod.ts";
 
 await build();
 await deploy();
